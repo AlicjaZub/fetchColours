ed493fcc0aa25eac8524cfec559e994ae6400821
Alicja <puranut@hotmail.com>
2021-06-17 15:06:42 +0100

    pagination added

--- script.js
+++ script.js
@@ -2,42 +2,65 @@ fetch('https://jsonplaceholder.typicode.com/photos')
     .then (response =>{
         response.json()
     .then (images => {
+        allColours = images
         let imagesChosen = images.slice(0, 30);
         renderImg(imagesChosen)
-        document.querySelectorAll('.images').forEach(image => {
-            image.addEventListener('click', () => {
-                content.innerHTML = '<img src="' + image.dataset.image + '">'
-                caption.innerHTML = image.dataset.title
-                modal.style.display = "block";
-            })
-        })
+        displayImages()
     })
     })
 
+let allColours
 const modal = document.querySelector('#myModal')
 const span = document.querySelector(".close");
 const content = document.querySelector('#content')
 const caption = document.querySelector('#caption')
-
+const nextButton = document.querySelector('#nextColours')
+const previousButton = document.querySelector('#previousColours')
+let page_number = 1
+const result = document.querySelector('#result')
 
 function renderImg(images) {
-    const location = document.querySelector('#result')
     images.forEach(image => {
         console.log(image)
-        location.innerHTML += '<div class="images" data-image="' + image.url + '" data-title="' + image.title + '"><img src="' + image.thumbnailUrl + '"></div>'
+        result.innerHTML += '<div class="images" data-image="' + image.url + '" data-title="' +
+            image.title + '"><img src="' + image.thumbnailUrl + '"></div>'
     });
 }
-
 
 span.addEventListener('click', e => {
     modal.style.display = "none";
 })
 
+nextButton.addEventListener('click', e => {
+    page_number++
+    if (page_number === 167) {
+        nextButton.style.display = 'none'
+    }
+    previousButton.style.display = 'block'
+    let imagesChosen = allColours.slice((page_number - 1) * 30, page_number * 30);
+    result.innerHTML = ''
+    renderImg(imagesChosen)
+    displayImages()
+})
 
+previousButton.addEventListener('click', e => {
+    page_number--
+    if (page_number === 1) {
+        previousButton.style.display = 'none'
+    }
+    let imagesChosen = allColours.slice((page_number - 1) * 30, page_number * 30);
+    result.innerHTML = ''
+    renderImg(imagesChosen)
+    displayImages()
+    window.scrollTo(0,0)
+})
 
-
-// Fetch the data above, and display the titles and the thumbnails in a
-// grid on a page.When the user clicks on any title or image, it should display the large
-// version of the image in a pop-up over the page (a modal)
-// there is 5000 objects in the data so you will probably want to reduce the array to only show ~100
-// or the page will load really slowly
+function displayImages() {
+    document.querySelectorAll('.images').forEach(image => {
+        image.addEventListener('click', () => {
+            content.innerHTML = '<img src="' + image.dataset.image + '">'
+            caption.innerHTML = image.dataset.title
+            modal.style.display = "block";
+        })
+    })
+}
